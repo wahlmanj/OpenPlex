1457f41a4a699439d8a53a14e3c8db90c0722cb8
wahlmanj <wahlman.j@gmail.com>
2015-01-10 00:50:50 -0600

    Update restoreatvsettings.bash

--- update/OSX/restoreatvsettings.bash
+++ update/OSX/restoreatvsettings.bash
@@ -1,9 +1,15 @@
+restart.bash
 stop.bash
-start.bash
 cp /Applications/plexconnect_BACKUP/ATVSettings.cfg /Applications/PlexConnect
 cd /Applications/PlexConnect
 chown root:admin ATVSettings.cfg
 chmod 644 ATVSettings.cfg
+while : ; do
+    [[ -f "/Applications/PlexConnect/ATVSettings.cfg" ]] && break
+    echo "Pausing until ATVSettings.cfg exists."
+    sleep 1
+done
+start.bash
 
 ## Display ATVSettings.cfg
 FILE="/Applications/PlexConnect/ATVSettings.cfg"
