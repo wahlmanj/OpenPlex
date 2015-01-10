f151f65438a4fb5664cfaddaf66a477b51e82c73
wahlmanj <wahlman.j@gmail.com>
2015-01-10 00:43:30 -0600

    Update restoreatvsettings.bash

--- update/OSX/restoreatvsettings.bash
+++ update/OSX/restoreatvsettings.bash
@@ -1,9 +1,4 @@
 stop.bash
-while : ; do
-    [[ -f "/Applications/PlexConnect/ATVSettings.cfg" ]] && break
-    echo "Pausing until ATVSettings.cfg exists."
-    sleep 1
-done
 start.bash
 cp /Applications/plexconnect_BACKUP/ATVSettings.cfg /Applications/PlexConnect
 cd /Applications/PlexConnect
