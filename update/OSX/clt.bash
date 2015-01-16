2f569e89f96774fbf49f28b380c0d41f1d7cdad0
wahlmanj <wahlman.j@gmail.com>
2015-01-15 18:53:41 -0600

    Update clt.bash

--- update/OSX/clt.bash
+++ update/OSX/clt.bash
@@ -4,64 +4,72 @@ OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
 if [ "$OSX_VERS" -eq 6 ]; then
 killall OpenPlex
 cd /Applications
-rm -Rf OpenPlex.app; sleep 2
-cp ~/Library/Application\ Support/OpenPlex/10.7/OpenPlex.zip /Applications; sleep 2
-unzip OpenPlex.zip; sleep 2
+rm -Rf OpenPlex.app
+cp ~/Library/Application\ Support/OpenPlex/10.7/OpenPlex.zip /Applications
+unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
+killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 7 ]; then
 killall OpenPlex
 cd /Applications
-rm -Rf OpenPlex.app; sleep 2
-cp ~/Library/Application\ Support/OpenPlex/10.7/OpenPlex.zip /Applications; sleep 2
-unzip OpenPlex.zip; sleep 2
+rm -Rf OpenPlex.app
+cp ~/Library/Application\ Support/OpenPlex/10.7/OpenPlex.zip /Applications
+unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
+killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 8 ]; then
 killall OpenPlex
 cd /Applications
-rm -Rf OpenPlex.app; sleep 2
-cp ~/Library/Application\ Support/OpenPlex/10.7/OpenPlex.zip /Applications; sleep 2
-unzip OpenPlex.zip; sleep 2
+rm -Rf OpenPlex.app
+cp ~/Library/Application\ Support/OpenPlex/10.7/OpenPlex.zip /Applications
+unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
+killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 9 ]; then
 killall OpenPlex
 cd /Applications
-rm -Rf OpenPlex.app; sleep 2
-cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications; sleep 2
-unzip OpenPlex.zip; sleep 2
+rm -Rf OpenPlex.app
+cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications
+unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
+killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 10 ]; then
 killall OpenPlex
 cd /Applications
-rm -Rf OpenPlex.app; sleep 2
-cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications; sleep 2
-unzip OpenPlex.zip; sleep 2
+rm -Rf OpenPlex.app
+cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications
+unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
+killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 11 ]; then
 killall OpenPlex
 cd /Applications
-rm -Rf OpenPlex.app; sleep 2
-cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications; sleep 2
-unzip OpenPlex.zip; sleep 2
+rm -Rf OpenPlex.app
+cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications
+unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
+killall updater
+echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 12 ]; then
 killall OpenPlex
 cd /Applications
-rm -Rf OpenPlex.app; sleep 2
-cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications; sleep 2
-unzip OpenPlex.zip; sleep 2
+rm -Rf OpenPlex.app
+cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications
+unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
+killall updater
 echo $OSX_VERS
 fi
 
