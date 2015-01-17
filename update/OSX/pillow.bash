a1375c69c75f561226f99eb52c220a315a7afbeb
wahlmanj <wahlman.j@gmail.com>
2015-01-16 20:49:11 -0600

    Update pillow.bash

--- update/OSX/pillow.bash
+++ update/OSX/pillow.bash
@@ -3,6 +3,8 @@
 cd /Applications/PlexConnect/update/OSX
 yes | ./pillow
 
-echo 'You need to choose a theme to install pillow'
-echo 'If you already installed a theme ignore this message'
+echo 'If you encounter errors with pillow not working try these commands in a terminal'
+echo 'sudo /usr/bin/easy_install-2.7 pip'
+echo 'sudo /usr/bin/easy-install-2.7 pillow'
+echo 'Ignore this fix if the above says pillow was successfully installed'
 echo 'Close the terminal window to continue installing...'
