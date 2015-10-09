10c1c4587666e6fa872194661caf23aa62033071
wahlmanj <wahlman.j@gmail.com>
2015-10-08 19:16:56 -0500

    optimize & fix bug in myplex loader

--- update/OSX/atvsettingsauto.bash
+++ update/OSX/atvsettingsauto.bash
@@ -1,23 +1,19 @@
 start.bash
 stop.bash
-sleep 4
 while : ; do
     [[ -f "/Applications/PlexConnect/ATVSettings.cfg" ]] && break
     echo "Pausing until ATVSettings.cfg exists."
-    sleep 4
+    sleep 1
     restart.bash
 done
 stop.bash
 sleep 3
-cp /Applications/PlexConnect/ATVSettings.cfg /Applications/plexconnect_BACKUP/ATVSettingsauto.cfg
-cd /Applications/plexconnect_BACKUP
-grep -B2 myplex_auth ATVSettings.cfg | sed '/^--$/d' > myplex.temp
-sed '1,3d' myplex.temp > myplex.auto
-rm myplex.temp
-cat myplex.auto >> ATVSettingsauto.cfg
-cp /Applications/plexconnect_BACKUP/ATVSettingsauto.cfg /Applications/PlexConnect/ATVSettings.cfg
-rm ATVSettingsauto.cfg
+cp /Applications/plexconnect_BACKUP/ATVSettings.cfg /Applications/PlexConnect
 cd /Applications/PlexConnect
 chown root:admin ATVSettings.cfg
 chmod 644 ATVSettings.cfg
 start.bash
+## Display ATVSettings.cfg
+FILE="/Applications/PlexConnect/ATVSettings.cfg"
+echo "*** File - $FILE contents ***"
+cat $FILE
