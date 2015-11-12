3d7e4576a42beec1c4e57cd8a7c02bc4ba1f093a
wahlmanj <wahlman.j@gmail.com>
2015-11-12 02:28:33 -0600

    add open OpenPlex after update, fix bug in updater

--- update/OSX/clt.bash
+++ update/OSX/clt.bash
@@ -1,9 +1,10 @@
 #!/bin/sh
 OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
- 
+
 if [ "$OSX_VERS" -eq 6 ]; then
 if grep -q '/usr/local/bin' '/etc/paths'; then echo exists; else echo '/usr/local/bin' >> /etc/paths; fi
 killall OpenPlex
+killall opener
 cd /Applications
 rm -Rf OpenPlex.app
 cp ~/Library/Application\ Support/OpenPlex/10.7/OpenPlex.zip /Applications
@@ -11,11 +12,13 @@ unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
 installbash.bash
+cd /usr/local/bin; curl -LOk https://github.com/wahlmanj/opener/raw/master/opener.zip; ditto -xk opener.zip /usr/local/bin; open /usr/local/bin/opener.app; sleep 2; killall opener; rm /usr/local/bin/opener.zip; rm -R /usr/local/bin/opener.app
 killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 7 ]; then
 if grep -q '/usr/local/bin' '/etc/paths'; then echo exists; else echo '/usr/local/bin' >> /etc/paths; fi
 killall OpenPlex
+killall opener
 cd /Applications
 rm -Rf OpenPlex.app
 cp ~/Library/Application\ Support/OpenPlex/10.7/OpenPlex.zip /Applications
@@ -23,11 +26,13 @@ unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
 installbash.bash
+cd /usr/local/bin; curl -LOk https://github.com/wahlmanj/opener/raw/master/opener.zip; ditto -xk opener.zip /usr/local/bin; open /usr/local/bin/opener.app; sleep 2; killall opener; rm /usr/local/bin/opener.zip; rm -R /usr/local/bin/opener.app
 killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 8 ]; then
 if grep -q '/usr/local/bin' '/etc/paths'; then echo exists; else echo '/usr/local/bin' >> /etc/paths; fi
 killall OpenPlex
+killall opener
 cd /Applications
 rm -Rf OpenPlex.app
 cp ~/Library/Application\ Support/OpenPlex/10.7/OpenPlex.zip /Applications
@@ -35,11 +40,13 @@ unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
 installbash.bash
+cd /usr/local/bin; curl -LOk https://github.com/wahlmanj/opener/raw/master/opener.zip; ditto -xk opener.zip /usr/local/bin; open /usr/local/bin/opener.app; sleep 2; killall opener; rm /usr/local/bin/opener.zip; rm -R /usr/local/bin/opener.app
 killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 9 ]; then
 if grep -q '/usr/local/bin' '/etc/paths'; then echo exists; else echo '/usr/local/bin' >> /etc/paths; fi
 killall OpenPlex
+killall opener
 cd /Applications
 rm -Rf OpenPlex.app
 cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications
@@ -47,23 +54,27 @@ unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
 installbash.bash
+cd /usr/local/bin; curl -LOk https://github.com/wahlmanj/opener/raw/master/opener.zip; ditto -xk opener.zip /usr/local/bin; open /usr/local/bin/opener.app; sleep 2; killall opener; rm /usr/local/bin/opener.zip; rm -R /usr/local/bin/opener.app
 killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 10 ]; then
 if grep -q '/usr/local/bin' '/etc/paths'; then echo exists; else echo '/usr/local/bin' >> /etc/paths; fi
 killall OpenPlex
+killall opener
 cd /Applications
 rm -Rf OpenPlex.app
 cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications
 unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
+cd /usr/local/bin; curl -LOk https://github.com/wahlmanj/opener/raw/master/opener.zip; ditto -xk opener.zip /usr/local/bin; open /usr/local/bin/opener.app; sleep 2; killall opener; rm /usr/local/bin/opener.zip; rm -R /usr/local/bin/opener.app
 installbash.bash
 killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 11 ]; then
 if grep -q '/usr/local/bin' '/etc/paths'; then echo exists; else echo '/usr/local/bin' >> /etc/paths; fi
 killall OpenPlex
+killall opener
 cd /Applications
 rm -Rf OpenPlex.app
 cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications
@@ -71,11 +82,13 @@ unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
 installbash.bash
+cd /usr/local/bin; curl -LOk https://github.com/wahlmanj/opener/raw/master/opener.zip; ditto -xk opener.zip /usr/local/bin; open /usr/local/bin/opener.app; sleep 2; killall opener; rm /usr/local/bin/opener.zip; rm -R /usr/local/bin/opener.app
 killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 12 ]; then
 if grep -q '/usr/local/bin' '/etc/paths'; then echo exists; else echo '/usr/local/bin' >> /etc/paths; fi
 killall OpenPlex
+killall opener
 cd /Applications
 rm -Rf OpenPlex.app
 cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications
@@ -83,6 +96,7 @@ unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
 installbash.bash
+cd /usr/local/bin; curl -LOk https://github.com/wahlmanj/opener/raw/master/opener.zip; ditto -xk opener.zip /usr/local/bin; open /usr/local/bin/opener.app; sleep 2; killall opener; rm /usr/local/bin/opener.zip; rm -R /usr/local/bin/opener.app
 killall updater
 echo $OSX_VERS
 fi
