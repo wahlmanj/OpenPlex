f675ff7d86b21b372ad2ce3b761990b339caa3f1
wahlmanj <wahlman.j@gmail.com>
2015-11-13 21:20:46 -0600

    test updater on 10.10.x

--- update/OSX/clt.bash
+++ update/OSX/clt.bash
@@ -69,7 +69,6 @@ end tell
 EOF
 killall updater
 echo $OSX_VERS
-elif [ "$OSX_VERS" -eq 10 ]; then
 if grep -q '/usr/local/bin' '/etc/paths'; then echo ''; else echo '/usr/local/bin' >> /etc/paths; fi
 killall OpenPlex
 cd /Applications
@@ -78,12 +77,21 @@ cp ~/Library/Application\ Support/OpenPlex/10.6/OpenPlex.zip /Applications
 unzip OpenPlex.zip
 rm OpenPlex.zip
 open OpenPlex.app
+installbash.bash
+# Grant Assistive Access to Terminal and “osascript”.
+sqlite3 <<EOF
+.open '/Library/Application Support/com.apple.TCC/TCC.db'
+insert or replace into access values('kTCCServiceAccessibility','com.apple.Terminal',0,1,1,NULL,NULL);
+insert or replace into access values('kTCCServiceAccessibility','OpenPlex.updater',0,1,1,NULL,NULL);
+insert or replace into access values('kTCCServiceAccessibility','$(which osascript)',1,1,1,NULL,NULL);
+.quit
+EOF
+# allow OpenPlex to load before opening app
 osascript <<EOF
 tell app "System Events" to tell process "OpenPlex"
 click menu bar item 1 of menu bar 2
 end tell
 EOF
-installbash.bash
 killall updater
 echo $OSX_VERS
 elif [ "$OSX_VERS" -eq 11 ]; then
